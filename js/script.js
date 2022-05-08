3687bb587fcea71241919be3612d1575fe218bf6
RENZOZ78 <didierbap@gmail.com>
2022-05-09 01:38:16 +0200

    shopping cart (product-container/cart-total)

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//search form
+let searchForm = document.querySelector('.search-form-container');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+// shopping cart
+let cart = document.querySelector('.shopping-cart-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
